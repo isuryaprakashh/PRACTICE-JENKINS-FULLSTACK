78085ba8ffff4d865312b2f54f7d6e28c4dfd60d
SURYA PRAKASH <isurya2005@gmail.com>
2025-09-08 14:32:54 +0530

    added extends SpringBootServletInitializer

--- PracSpringBootJenkinsBackendProject/src/main/java/com/jenkins/PracSpringBootJenkinsBackendProjectApplication.java
+++ PracSpringBootJenkinsBackendProject/src/main/java/com/jenkins/PracSpringBootJenkinsBackendProjectApplication.java
@@ -2,9 +2,10 @@ package com.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PracSpringBootJenkinsBackendProjectApplication {
+public class PracSpringBootJenkinsBackendProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracSpringBootJenkinsBackendProjectApplication.class, args);
